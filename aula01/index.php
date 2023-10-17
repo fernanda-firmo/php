3b82a3d70009267a5a2de6e9c379e594280ba436
Maria Fernanda Barbosa Firmo <145458198+fernanda-firmo@users.noreply.github.com>
2023-10-17 11:20:37 -0300

    Add files via upload
    Aula 01

---
+++ aula01/index.php
@@ -0,0 +1,18 @@
+<?php
+   echo 'teste impressao php';
+
+   echo'<br>';
+   echo'<hr>';
+
+?>
+<?= " Mas uma nova linha agora"?>
+
+
+<?php
+// echo "comentario de uma linha";
+/*comentario de varias linhas
+    nova linhas
+    nova linhas
+    */
+    echo "teste";
+    ?>
